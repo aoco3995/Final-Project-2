90482452a77399e3e610eeb2709b844ec49fc5b0
aoco3995 <adam.oconnor32@gmail.com>
2025-04-18 21:26:21 +0000

    broke out data_loader

--- Code/train_model.py
+++ Code/train_model.py
@@ -1,59 +1,12 @@
-import os
-import json
-import random
-import cv2
-import pandas as pd
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils import data
 from sklearn.preprocessing import MultiLabelBinarizer
-from torchvision import transforms
-from tqdm import tqdm
 from model import CNN  # Import the CNN model from model.py
 from config import *
 from metrics import evaluate_metrics  # Import the metrics from metrics.py
-
-
-# Load JSON annotations
-def load_json_annotations(json_folder):
-    rows = []
-    for fname in os.listdir(json_folder):
-        if fname.endswith(".json"):
-            with open(os.path.join(json_folder, fname), "r") as f:
-                data = json.load(f)
-                for item in data:
-                    image = item["image"]
-                    for ann in item["annotations"]:
-                        label = ann["label"]
-                        rows.append({"id": image, "target": label})
-    return pd.DataFrame(rows)
-
-# Dataset Class
-class CustomDataset(data.Dataset):
-    def __init__(self, df, label_map):
-        self.df = df
-        self.label_map = label_map
-        self.transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-            transforms.ToTensor()
-        ])
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        row = self.df.iloc[idx]
-        img_path = os.path.join(DATA_DIR, row['id'])
-        image = cv2.imread(img_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = self.transform(image)
-        target = [0] * len(self.label_map)
-        for label in row['target'].split(','):
-            if label in self.label_map:
-                target[self.label_map[label]] = 1
-        return image, torch.FloatTensor(target)
+from data_loader import load_json_annotations, CustomDataset  # Import the data loader functions
 
 
 # Main training loop
