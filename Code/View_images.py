e08e3645ce69213ebd67eaa2ec435e1ece408915
aoco3995 <30965765+aoco3995@users.noreply.github.com>
2025-05-04 16:07:10 -0500

    Modified View_images.py

--- Code/View_images.py
+++ Code/View_images.py
@@ -7,7 +7,7 @@ DATA_DIR = "Dataset/Aircraft_Fuselage_DET2023/unlabel_aircraft_fuselage"
 csv_path = "pseudo_labels_MARS.csv"
 
 # Set the label you're interested in (case-insensitive match)
-TARGET_LABEL = "scratch".lower()
+TARGET_LABEL = "paint peel".lower()
 
 # Load and filter CSV entries
 with open(csv_path, newline='') as f:
